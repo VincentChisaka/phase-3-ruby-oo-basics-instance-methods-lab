304014ba26e9a9ababf7bd24d82337aa225bd753
VincentChisaka <vincentchisaka2@gmail.com>
2023-02-10 20:50:40 +0300

    done lab

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+    
+    def bark
+      puts "Woof!"
+    end
+
+    def sit
+      puts "The Dog is sitting"
+  end
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
